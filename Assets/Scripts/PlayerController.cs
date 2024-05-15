4317f1eaf34c152f0ad7112ec3ab8b0fb8a266d2
Võ Minh Hoàng <vominhhoangkhung@gmail.com>
2024-05-15 09:14:10 +0700

    update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ public class PlayerController : MonoBehaviour
 {
     public float gridSize = 1f;
     private Vector2Int currentCell;
-
+    public LayerMask collisionMask;
     void Start()
     {
 
@@ -16,6 +16,7 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
         {
+
             MovePlayer(Vector2Int.right);
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
@@ -34,14 +35,24 @@ public class PlayerController : MonoBehaviour
 
     public void MovePlayer(Vector2Int direction)
     {
-
         Vector2Int newCell = currentCell + direction;
         Vector3 newPosition = new Vector3((newCell.x + 0.5f) * gridSize, (newCell.y + 0.5f) * gridSize, 0f);
 
-        Debug.Log("test2");
-        transform.position = newPosition;
 
+        if (!IsCellBlocked(newPosition, direction))
+        {
+            transform.position = newPosition;
+            currentCell = newCell;
+        }
+    }
 
-        currentCell = newCell;
+    private bool IsCellBlocked(Vector3 newPosition, Vector2Int direction)
+    {
+        Vector2 origin = new Vector2(transform.position.x, transform.position.y);
+        Vector2 destination = new Vector2(newPosition.x, newPosition.y);
+        Vector2 directionVector = destination - origin;
+
+        RaycastHit2D hit = Physics2D.Raycast(origin, directionVector, directionVector.magnitude, collisionMask);
+        return hit.collider != null;
     }
 }
