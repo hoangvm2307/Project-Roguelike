65de068d67f1d56d8dbdbd081bc912c709652081
Võ Minh Hoàng <vominhhoangkhung@gmail.com>
2025-03-11 14:39:55 +0700

    update

--- Assets/Scripts/MinimapScripts/MinimapController.cs
+++ Assets/Scripts/MinimapScripts/MinimapController.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Build.Content;
 using UnityEngine;
-
+using UnityEditor;
 public class MinimapController : MonoBehaviour
 {
     public static MinimapController instance;
